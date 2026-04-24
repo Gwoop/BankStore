c4709650fbd46986c951705dd94497a88bf20f7a
Gwoop <gwooparap@gmail.com>
2026-04-24 09:48:18 +0300

    Реализация CRUD Entry

--- api/entry.go
+++ api/entry.go
@@ -14,6 +14,11 @@ type createEntryRequest struct {
 	Amount    int64 `json:"amount" binding:"required"`
 }
 
+type updateEntryRequest struct {
+	AmountID int64 `json:"amountid" binding:"required"`
+	Amount   int64 `json:"amount" binding:"required"`
+}
+
 type createEntryResponse struct {
 	AccountID int64 `json:"accountid"`
 }
@@ -25,8 +30,7 @@ func (server *Server) CreateEntry(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
-	// получаем hash паролья по строке пароля
-	// создаем структуру для запроса на создание нового пользователя в БД
+
 	arg := db.CreateEntryParams{
 		AccountID: req.AccountID,
 		Amount:    req.Amount,
@@ -51,4 +55,55 @@ func (server *Server) CreateEntry(ctx *gin.Context) {
 		AccountID: entry.AccountID,
 	}
 	ctx.JSON(http.StatusOK, rsp)
+}
+
+type delteEntryRQ struct {
+	AccountID int64 `json:"accountid"`
+}
+
+func (server *Server) DeleteEntry(ctx *gin.Context) {
+	var req delteEntryRQ
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err := server.store.DeleteEntry(ctx, req.AccountID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// сформировать структуру для ответа
+
+	ctx.JSON(http.StatusOK, gin.H{"response": "Успешное удаление"})
+}
+
+func (server *Server) UpdateEntry(ctx *gin.Context) {
+	var req updateEntryRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.UpdateEntryParams{
+		ID:     req.AmountID,
+		Amount: req.Amount,
+	}
+	entry, err := server.store.UpdateEntry(ctx, arg)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			switch pgErr.Code {
+			case "23505":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"response": entry.ID})
 }
